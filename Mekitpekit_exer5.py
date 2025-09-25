7d882df6964bcbb4c61271e819f65e33f2e8eee1
Edmarc Mekitpekit <mekitpekit.eggmarc@gmail.com>
2025-09-25 09:58:36 +0800

    exer5

--- Mekitpekit_exer5.py
+++ Mekitpekit_exer5.py
@@ -1,3 +1,9 @@
+
+
+
+
+
+
 import random
 def toLower(str):
     #make strings with similar index for similar chars
@@ -23,8 +29,10 @@ def toLower(str):
     
     return lowered
 
-def game():
+#main menu
+def game():    
     while True:
+        print()
         print("===== welcome to Terminal Wordle =====")
         print()
         print("\t[1] Play Game")
@@ -38,52 +46,53 @@ def game():
         elif choice == 2:
             managePool()
         elif choice == 0:
-            print("Goodbye!")
+            print("I hope you had fun!")
             break
         else:
             print("Please choose from the choices only.")
   
+#wordle game
 def playGame():  
     copy_words = words.copy()
+    if len(copy_words) == 0:
+        print("Word pool is empty. Add words to play the game.")
+        return
     while True:  
-
         attempts = 1
         random_word = random.choice(copy_words)
-        random_display = list("_" * len(random_word))
-        
+        random_display = list("_" * len(random_word)) # generates a list with of "_" with length of random word
         while True:
-            
+            print()
             print("===== Attempt", attempts, "=====")
             print("\t", end="")
             for i in random_display:
-                print(i, end=" ")
+                print(i, end=" ") #displays the "_"
             
             print()
 
-            guess = input("Guess: ")
-
-            if guess == random_word:
+            guess = toLower(input("Guess: ")) # get guess
+            if guess == random_word: # check if guess is correct 
                 print("============================")
                 print("\t", guess)
-                print("So good, you got it in", attempts, "attempts")
+                print("So good, you got it in", attempts, "attempt(s)")
                 choice = input("Do you want to play again? (y/n): ")
                 if choice == "y":
                     copy_words.remove(random_word)
                     if len(copy_words) == 0:
-                        print("No words left in the word pool.")
-                        return
-                    break
+                        print("No words left in the word pool. Play again latur.")
+                        return # returns to the main menu
+                    break # breaks out of the inner while loop and starts a new game
                 else:
-                    return
+                    return 
             else: 
-                for i in range(len(random_display)):
-                    if guess[i] in random_word:
-                        if guess[i] == random_word[i]:
-                            random_display[i] = guess[i]
+                for i in range(len(random_display)): 
+                    if guess[i] in random_word: # check if the letter in guess is in the random word
+                        if guess[i] == random_word[i]: # check if the letter is in the right index
+                            random_display[i] = guess[i] # replace the "_" in the right index with the correct letter
                         else:
-                            random_display[i] = "*"
+                            random_display[i] = "*" # if the letter is in the wrong index, replace "_" with "*"
                     else:
-                        random_display[i] = "-"
+                        random_display[i] = "-" # if the letter is not in the random word at all
 
             attempts += 1
 
@@ -91,6 +100,7 @@ def playGame():
 def managePool():
      global words
      while True:
+        print()
         print("===== Manage Word Pool =====")
         print()
         print("\t[1] Print Word Pool")
@@ -104,11 +114,13 @@ def managePool():
         if choice == 1:
             print("Words: ")
             for i in words:
-                print("\t", i)
+                print("\t", i) # loop and print each in the list
+        
         elif choice == 2:
             new_word = toLower(input("Enter new word to insert: "))
             words.append(new_word)
             print(new_word,"inserted.")
+        
         elif choice == 3:
             del_word = toLower(input("Enter word to delete: "))
             if len(words) == 0:
@@ -118,15 +130,18 @@ def managePool():
                 print(del_word, "deleted.")
             else:
                 print(del_word, "is not in the word pool.")
+        
         elif choice == 4:
             words = []
             print("Word pool cleared.")
+        
         elif choice == 0:
-            print("Have fun!")
+            print("Have fun playing!")
             break
+        
         else:
             print("Please choose from the choices only.")
 
-words = ['keep', 'feel', 'reel']
+words = ['keep', 'feel', 'reel', 'reap', 'flow']
 
 game()
